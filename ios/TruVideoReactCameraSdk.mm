30e8691128e61d183decaa85dc32edc5de81a94b
BhaveshPathe <bhaveshpathe@BhaveshPatheMacAir.local>
2025-12-16 15:11:26 +0530

    feat(ios): add AR camera and scanner exports
    Expose AR camera, environment, and AR support methods in iOS bridge.
    Update example Xcode project settings.

--- ios/TruVideoReactCameraSdk.mm
+++ ios/TruVideoReactCameraSdk.mm
@@ -5,7 +5,24 @@
 RCT_EXTERN_METHOD(initCameraScreen:(NSString)jsonData
                  withResolver:(RCTPromiseResolveBlock)resolve
                  withRejecter:(RCTPromiseRejectBlock)reject)
-                 
+
+RCT_EXTERN_METHOD(initARCameraScreen:(NSString)configuration
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(environment:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(isAugmentedRealityInstalled:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(isAugmentedRealitySupported:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(initScanerScreen:(NSString)configuration
+                 withResolver:(RCTPromiseResolveBlock)resolve
+                 withRejecter:(RCTPromiseRejectBlock)reject)
+
 + (BOOL)requiresMainQueueSetup
 {
   return NO;
