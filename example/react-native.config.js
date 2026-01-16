1d3e15db8e3a864da7db3383e1659dbcfa8798b1
BhaveshPathe <bhaveshpathe@BhaveshPatheMacAir.local>
2026-01-16 19:30:42 +0530

    chore: package update

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -1,4 +1,4 @@
-const path = require('path');
+const path = require('node:path');
 
 module.exports = {
   project: {
@@ -10,11 +10,25 @@ module.exports = {
     // Configure @trunpm/truvideo-react-camera-sdk to point to the npm package
     // Since this is a yarn workspace, the package is hoisted to root node_modules
     '@trunpm/truvideo-react-camera-sdk': {
-      root: path.join(__dirname, '..', 'node_modules', '@trunpm', 'truvideo-react-camera-sdk'),
+      root: path.resolve(
+        __dirname,
+        '..',
+        'node_modules',
+        '@trunpm',
+        'truvideo-react-camera-sdk'
+      ),
       platforms: {
         android: {
-          sourceDir: path.join(__dirname, '..', 'node_modules', '@trunpm', 'truvideo-react-camera-sdk', 'android'),
-          packageImportPath: 'import com.truvideoreactcamerasdk.TruVideoReactCameraSdkPackage;',
+          sourceDir: path.resolve(
+            __dirname,
+            '..',
+            'node_modules',
+            '@trunpm',
+            'truvideo-react-camera-sdk',
+            'android'
+          ),
+          packageImportPath:
+            'import com.truvideoreactcamerasdk.TruVideoReactCameraSdkPackage;',
         },
       },
     },
