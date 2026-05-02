6f62ff111d7d49a24afc5d99339b5604d411c37f
Himanshu <premanshu.solanki@cellulant.io>
2026-05-02 12:50:25 +0530

    TruvideoSdkNewArCameraContract issue fix in ArCameraActivity

--- android/src/main/java/com/truvideoreactcamerasdk/ArCameraActivity.kt
+++ android/src/main/java/com/truvideoreactcamerasdk/ArCameraActivity.kt
@@ -10,7 +10,7 @@ import com.truvideo.sdk.camera.TruvideoSdkCamera
 import com.truvideo.sdk.camera.model.TruvideoSdkArCameraConfiguration
 import com.truvideo.sdk.camera.model.external.TruvideoSdkCameraMode
 import com.truvideo.sdk.camera.model.TruvideoSdkCameraOrientation
-import com.truvideo.sdk.camera.ui.activities.arcamera.TruvideoSdkArCameraContract
+import com.truvideo.sdk.camera.ui.activities.proposal.TruvideoSdkNewArCameraContract
 import org.json.JSONArray
 import org.json.JSONObject
 
@@ -30,7 +30,7 @@ class ArCameraActivity : AppCompatActivity() {
         }
 
       val configuration = intent.getStringExtra("configuration")!!
-      launcher = registerForActivityResult(TruvideoSdkArCameraContract()){
+      launcher = registerForActivityResult(TruvideoSdkNewArCameraContract()){
         //val jsonArray = Json.encodeToString(ListSerializer(TruvideoSdkCameraMedia.serializer()),it)
         val jsonArray = JSONArray()
         it.forEach { media ->
